a7d64c9a15af19495a72deea5b0fa16d4294b426
Julián <juliandorao@gmail.com>
2023-08-31 20:54:54 -0300

    Codo a codo 4.0

---
+++ 02_Ejercicios sobre condicionales.py
@@ -0,0 +1,36 @@
+
+#AlgoritmodelUnoalTres
+
+# Condicionales if... else: // if... elif... else:
+
+num=int(input("Ingrese un número del uno al tres: "))
+
+
+if num==3:
+    print("Es tres")
+elif num == 2:
+    print ("Es dos")
+else:
+    print("Es uno")
+
+"""
+#Un flujo de selección doble:
+
+ a=10
+ b=5
+
+ if b!=o:
+     c=a/b
+ else: 
+     c=-1
+
+ print (c)
+
+
+# Un operador ternario, a partir del flujo de selección doble:
+
+c=a/b if b!=0 else -1
+print(c)
+
+
+"""
