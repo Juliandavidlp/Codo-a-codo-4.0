aa8b48c47133d9a944fba236ad520bbc01342a7d
Julián Dorao <133274347+Juliandavidlp@users.noreply.github.com>
2024-03-20 13:28:42 -0300

    Codo a codo 4.0

--- 03_Ejercicios cícliclos.py
+++ 03_Ejercicios cícliclos.py
@@ -1,48 +1,54 @@
 """
-# ¿Cómo recorrer una tupla con un for?
+¿Cómo recorrer una tupla con un for?
 
-# tupla=(0,1,2,3,4,5)
+ tupla=(0,1,2,3,4,5)
 
-# for i in (tupla):
-#     print(i)
+ for i in (tupla):
+     print(i)
 
-# Mostrar los números pares del uno al diez con un "para" usando la función range:
-lista=[1,2,3,4,5,6,7,8,9,10]
+ Mostrar los números pares del uno al diez en una lista con un "para" usando la función range:
+ lista=[1,2,3,4,5,6,7,8,9,10]
 
-for lista in range(0,11,2):
-    print(lista)
+ for lista in range(0,11,2):
+     print(lista)
+
+#Hacer un ciclo mientras contando hasta cinco:
+
+acumulador=0
+
+while acumulador <= 5:
+    print(acumulador)
+    acumulador+=1
+
     
+#La sintaxis es "while" + una expresión booleana + ":", primero hay que imprimir la variable 
+y después incrementar la variable acumuladora. Un contador cuenta eventos, mientras que una variable acumuladora acumula valores.
+Un contador se incrementa en 1, mientras que una variable acumuladora se actualiza con una operación matemática. El objetivo 
+de un contador es obtener un conteo, mientras que el objetivo de una variable acumuladora es obtener un resultado total.
 
-# ¿Cómo hacer un ciclo "mientras" en Python contando hasta cinco?
+#Un contador
 
-contador=0
+cont = 0
+for i in range(10):
+    if i % 2 == 0:
+        cont += 1
 
-while contador <=5:
-    print(contador)
-    contador+=1
-    
+print(f"Cantidad de números pares: {cont}")
 
-"""
+#Variable acumuladora
+
+suma = 0
+for i in range(1, 11):
+    suma += i
+
+print(f"Suma de los números del 1 al 10: {suma}")
 
 
 #Hacer dos ejercicios que impriman mediante un "for" y un "while" los números del diez al uno.
 
-"""
-variable=10
 
-for variable in range(10,0,-1):
+for variable in range (10,0, -1):
     print (variable)
-    
-
-
-
-variable=10
-
-while variable > 0:
-
-    print (variable)
-    variable= variable-1
-
 
 
 # Una variable acumuladora:
@@ -53,7 +59,6 @@ sumaVentas= sumaVentas + 4
 print(sumaVentas)
 
 
-
 # Se puede reescribir de esta manera...
 
 sumaVentas=4               
@@ -61,5 +66,10 @@ sumaVentas+=4
 
 print(sumaVentas)
 
-
 """
+variable=10
+
+while variable >= 0: 
+    print(variable)
+    variable-=1
+
